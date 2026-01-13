e86b3e8454b730736380df0524816a54cc62988f
Rudra Prasad <rudraprasad.email@gmail.com>
2026-01-13 12:02:24 +0530

    fix: Swapped Failtest and normal Deply

--- routes/init_routes.go
+++ routes/init_routes.go
@@ -21,7 +21,7 @@ func RegisterRoutes(r *gin.Engine, db *pgxpool.Pool) {
 
 	webhookRouter := api.Group("/webhook")
 	{
-		webhookRouter.POST("/deploy-fail", deployController.DeployWebhook)
-		webhookRouter.POST("/deploy", deployController.DeployWebhookFailTest)
+		webhookRouter.POST("/deploy", deployController.DeployWebhook)
+		webhookRouter.POST("/deploy-fail-test", deployController.DeployWebhookFailTest)
 	}
 }
